e8ce76275597b657677007b666ed3514a79ad1fe
Wilsonfb <150964778+Wilsonfb@users.noreply.github.com>
2024-05-09 18:15:38 -0500

    Add files via upload

---
+++ pro15.py
@@ -0,0 +1,10 @@
+#Variables
+nombre, edad, pronombre, pais, estudia = "Nixson Fabian", 18, "Fabis", "Colombia", "analista y desarrollador"
+print(f"Mi nombre es {nombre} estudio {estudia} con la edad de {edad} años naci en {pais} y la mayoria de las personas me llaman por {pronombre}.")
+
+nombre = input("Digite su nombre: ")
+edad = int(input("Digite su edad: "))
+pronombre = input("Digite su pronombre: ")
+pais = input("Digite su pais: ")
+estudia = input("Digite que estudia: ")
+print(f"Mi nombre es {nombre} estudio {estudia} con la edad de {edad} años naci en {pais} y la mayoria de las personas me llaman por {pronombre}.")
